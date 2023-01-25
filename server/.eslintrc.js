81d4568a30126c5927dc516d9fe0cc4db9a688b2
Ajay <dev@ajay.app>
2023-01-25 12:44:25 -0500

    Add eslint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,4 +1,20 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "indent": ["warn", 2, { "SwitchCase": 1 }],
+    "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 0 }],
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["warn", "always"],
+    "prefer-template": "warn",
+    "quotes": ["warn", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+    "require-await": "error",
+    "semi": "warn"
+  },
 };
