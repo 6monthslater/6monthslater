5e0785863b499d3844cb7bb0a4f64ab43735a0bf
Ajay <dev@ajay.app>
2023-01-25 12:37:34 -0500

    Init remix project in server folder

---
+++ server/app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to 6 Months Later</h1>
+    </div>
+  );
+}
