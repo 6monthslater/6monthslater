9948f9a3ab55de17345acf85287614886ef03923
Ajay <dev@ajay.app>
2023-02-23 02:18:18 -0500

    Add amazon review parsing
    Fix Amazon review parsing #2

--- scraper/requester/amazon.py
+++ scraper/requester/amazon.py
@@ -1,2 +1,27 @@
-def request_review(product_id: str) -> str:
-    return ""
+from requester.request_maker import request_page
+
+class AmazonRegion:
+    COM = 1
+    CA = 2
+
+def request_reviews(region: AmazonRegion, product_id: str, page = 0) -> str:
+    return request_page(url_for_reviews(region, product_id, page))
+
+def url_for_reviews(region: AmazonRegion, product_id: str, page = 0) -> str:
+    attributes = []
+    if page > 0:
+        attributes.append(f"pageNumber={page + 1}")
+    attributes_str = "" if len(attributes) == 0 else f"?{'&'.join(attributes)}"
+
+    return f"https://{domain_for_region(region)}/product-reviews/{product_id}/{attributes_str}"
+
+def url_for_review(region: AmazonRegion, review_id: str) -> str:
+    return f"https://{domain_for_region(region)}/gp/customer-reviews/{review_id}/"
+
+def domain_for_region(region: AmazonRegion) -> str:
+    if region == AmazonRegion.COM:
+        return "www.amazon.com"
+    elif region == AmazonRegion.CA:
+        return "www.amazon.ca"
+    else:
+        raise Exception(f"Unknown region: {region}")
---
+++ scraper/requester/request_maker.py
@@ -0,0 +1,39 @@
+import pycurl
+import certifi
+from io import BytesIO
+
+def request_page(url: str) -> str:
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, url)
+    c.setopt(pycurl.WRITEDATA, buffer)
+    # HTTPS root certs for verification
+    c.setopt(pycurl.CAINFO, certifi.where())
+
+    c.setopt(pycurl.HTTPHEADER, [
+        "authority: www.amazon.ca",
+        "accept-language: en-US,en;q=0.5",
+        "accept-encoding: gzip, deflate, br",
+        "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "sec-ch-ua: \"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"",
+        "sec-ch-ua-mobile: ?0",
+        "sec-ch-ua-platform: \"Windows\"",
+        "sec-fetch-dest: document",
+        "sec-fetch-mode: navigate",
+        "sec-fetch-site: none",
+        "sec-fetch-user: ?1",
+        "upgrade-insecure-requests: 1",
+        "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+    ])
+
+    c.setopt(pycurl.ENCODING, 'gzip, deflate') 
+
+    c.perform()
+
+    body = buffer.getvalue()
+    response_code = c.getinfo(pycurl.RESPONSE_CODE)
+    if response_code != 200:
+        raise Exception(f"Failed to fetch {url} with status code: {response_code}")
+
+    c.close()
+    return body.decode('utf-8')
