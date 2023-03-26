40ce82171471fbde3569cfa5513f57a9dd300e27
Eric C <39015447+ricetech@users.noreply.github.com>
2023-03-26 03:08:12 -0400

    build: remove ESLint rules that conflict with Prettier

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,20 +5,11 @@ module.exports = {
     "@remix-run/eslint-config/node",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   plugins: ["@typescript-eslint"],
   rules: {
-    indent: ["warn", 2, { SwitchCase: 1 }],
-    "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 0 }],
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["warn", "always"],
     "prefer-template": "warn",
-    quotes: [
-      "warn",
-      "double",
-      { avoidEscape: true, allowTemplateLiterals: true },
-    ],
     "require-await": "error",
-    semi: "warn",
   },
 };
