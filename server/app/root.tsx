33be614d306704703581c4251a1cd1ea2425cdbc
Eric C <39015447+ricetech@users.noreply.github.com>
2023-04-02 00:32:25 -0400

    feat: update app title in HTML meta

--- server/app/root.tsx
+++ server/app/root.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "6 Months Later",
   viewport: "width=device-width,initial-scale=1",
 });
 
