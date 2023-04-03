37fc2a3786368839de1f11c8af7950d9ad0ba7c2
Eric C <39015447+ricetech@users.noreply.github.com>
2023-04-03 01:12:39 -0400

    chore: create navbar file

---
+++ server/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="flex flex-wrap items-center justify-between bg-sky-600 p-6">
+      <div className="mr-6 flex flex-shrink-0 items-center">
+        <span className="text-xl font-semibold tracking-tight">
+          6 Months Later
+        </span>
+      </div>
+    </nav>
+  );
+};
