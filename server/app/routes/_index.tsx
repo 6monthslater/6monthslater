a5f5d20b7d023a3bb0b939ea408c268cf8db4eed
Eric C <39015447+ricetech@users.noreply.github.com>
2023-04-08 23:53:04 -0400

    feat: stylize index page header

--- server/app/routes/_index.tsx
+++ server/app/routes/_index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Welcome to 6 Months Later</h1>
+    <div className="h-full content-center items-center pt-[20vh] text-center md:container md:mx-auto md:flex md:columns-1">
+      <h1 className="w-full text-5xl font-bold">Welcome to 6 Months Later!</h1>
     </div>
   );
 }
