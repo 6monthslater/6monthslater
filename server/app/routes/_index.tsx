aecf61fb4fe1dad661956aead725d34945d0f5c0
Eric C <39015447+ricetech@users.noreply.github.com>
2023-04-10 23:21:37 -0400

    feat: add search box

--- server/app/routes/_index.tsx
+++ server/app/routes/_index.tsx
@@ -4,12 +4,14 @@ import Button from "~/components/button";
 
 export default function Index() {
   return (
-    <div className="h-full content-center items-center space-y-4 pt-[20vh] text-center md:container md:mx-auto">
+    <div className="mx-4 h-full content-center items-center space-y-4 pt-[20vh] text-center md:container md:mx-auto">
       <h1 className="w-full text-5xl font-bold">Welcome to 6 Months Later!</h1>
       <p>Start by searching for a product:</p>
-      <div className="mx-auto flex space-x-3 md:w-1/2">
-        <TextInput className="" placeholder="Search for a product..." />
-        <Button icon={TbSearch}>Search</Button>
+      <div className="mx-auto space-x-3 md:flex md:w-1/2">
+        <TextInput className="mb-3" placeholder="Search for a product..." />
+        <Button className="mb-3" icon={TbSearch}>
+          Search
+        </Button>
       </div>
     </div>
   );
