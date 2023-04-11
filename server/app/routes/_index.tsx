3564595e7599d5b65c411fc6254ca1b5b1d60aa5
Eric C <39015447+ricetech@users.noreply.github.com>
2023-04-10 23:06:47 -0400

    feat: add search box

--- server/app/routes/_index.tsx
+++ server/app/routes/_index.tsx
@@ -1,7 +1,16 @@
+import { TextInput } from "@tremor/react";
+import { TbSearch } from "react-icons/tb";
+import Button from "~/components/button";
+
 export default function Index() {
   return (
-    <div className="h-full content-center items-center pt-[20vh] text-center md:container md:mx-auto md:flex md:columns-1">
+    <div className="h-full content-center items-center space-y-4 pt-[20vh] text-center md:container md:mx-auto">
       <h1 className="w-full text-5xl font-bold">Welcome to 6 Months Later!</h1>
+      <p>Start by searching for a product:</p>
+      <div className="mx-auto flex space-x-3 md:w-1/2">
+        <TextInput className="" placeholder="Search for a product..." />
+        <Button icon={TbSearch}>Search</Button>
+      </div>
     </div>
   );
 }
