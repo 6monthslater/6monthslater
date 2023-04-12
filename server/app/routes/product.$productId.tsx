d855f001c4ef092db57fc4b5dfd64a08541a6f08
Eric C <39015447+ricetech@users.noreply.github.com>
2023-04-12 14:34:43 -0400

    feat: add reliability score dummy card

--- server/app/routes/product.$productId.tsx
+++ server/app/routes/product.$productId.tsx
@@ -2,7 +2,7 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 import { useEffect, useState } from "react";
-import { BarList, Card, Title } from "@tremor/react";
+import { BarList, Card, DonutChart, Title } from "@tremor/react";
 import type { TremorBarListData } from "~/types/TremorBarListData";
 
 export const loader = async ({ params }: { params: { productId: string } }) => {
@@ -52,10 +52,22 @@ export default function Route() {
   return (
     <div className="mx-4 h-full grow content-center items-center space-y-4 py-4 text-center md:container md:mx-auto">
       <h1 className="text-xl font-semibold">{product?.name}</h1>
-      <div className="mx-auto md:w-1/2">
+      <div className="mx-auto space-y-4 md:w-3/4 md:columns-md">
         <Card>
           <Title className="font-semibold">Top Issues</Title>
           <BarList data={issueBarChartData} />
+        </Card>
+        <Card>
+          <Title className="font-semibold">Reliability Score</Title>
+          <DonutChart
+            data={[
+              { name: "Score", value: 8.5 },
+              { name: "Remainder", value: 2.5 },
+            ]}
+            label="8.5"
+            showLabel
+            colors={["cyan", "slate"]}
+          />
         </Card>
       </div>
       <div>
