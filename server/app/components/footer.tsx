9527d1988f6af2df8a891c9f88b7038bc4c2a4ac
Ajay <dev@ajay.app>
2023-04-13 13:36:44 -0400

    Better footer

--- server/app/components/footer.tsx
+++ server/app/components/footer.tsx
@@ -1,7 +1,16 @@
 const Footer = () => {
   return (
-    <div className="!mt-auto flex flex-wrap justify-center bg-gray-300 p-4">
-      <span>© 2023 6 Months Later</span>
+    <div className="!mt-auto bg-gray-300 p-4 text-center">
+      <div className="pb-4">A long term reliability assesment assistant</div>
+      <div>
+        6 Months Later is licensed under AGPLv3, available on{" "}
+        <a
+          href="https://www.github.com/6monthslater/6monthslater"
+          rel="noopener"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   );
 };
