5d0f843241a3288b3fe4dc950260d6560bf45a52
Guy-L <g.guy.lab@gmail.com>
2023-09-29 20:32:52 -0400

    Removed confirmation prompt for CI

--- scraper/setup.sh
+++ scraper/setup.sh
@@ -2,4 +2,3 @@
 # Sets up SUTime by installing wrapped jars + textblob corpora for training classifiers
 cd analyzer
 mvn dependency:copy-dependencies -DoutputDirectory=./jars
-read -p "Press Enter to exit script"
