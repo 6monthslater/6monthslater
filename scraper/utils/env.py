d1c68865971830f976beb6710f501157d3d11366
Ajay <dev@ajay.app>
2023-10-13 19:21:12 -0400

    Add default cookie for running tests with an empty config

--- scraper/utils/env.py
+++ scraper/utils/env.py
@@ -3,5 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+defaults = {
+    "AMAZON_COOKIE": "csm-sid=568-8411815-1797105; session-id=140-6102862-5001741; session-id-time=2082787201l"
+}
+
 def get_env(name: str) -> str | None:
     return os.getenv(name)
