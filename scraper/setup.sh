9d0ba996aaa9ac339655645dd406521c1d7bd9a8
Ajay <dev@ajay.app>
2023-11-15 18:13:16 -0500

    Add training system using LLaMa
    This can be used to build up a training data set for our analyzer

--- scraper/setup.sh
+++ scraper/setup.sh
@@ -2,3 +2,6 @@
 # Sets up SUTime by installing wrapped jars + textblob corpora for training classifiers
 cd analyzer
 mvn dependency:copy-dependencies -DoutputDirectory=./jars
+
+cd ../models
+wget https://huggingface.co/TheBloke/CodeLlama-7B-Instruct-GGUF/resolve/main/codellama-7b-instruct.Q5_K_S.gguf
