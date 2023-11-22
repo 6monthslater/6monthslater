5ae77599ecccbf355c74362fd36f5a5e47ec0c63
Ajay <dev@ajay.app>
2023-11-22 11:51:40 -0500

    Remove duplicate value set in search combo box

--- server/app/routes/_index.tsx
+++ server/app/routes/_index.tsx
@@ -134,7 +134,6 @@ export default function Index() {
                 className="tremor-TextInput-input w-full border-0 bg-transparent py-2 pl-4 pr-4 text-sm font-medium placeholder:text-gray-500 focus:outline-none focus:ring-0"
                 placeholder="Search for a product..."
                 name="productName"
-                value={searchProdName}
                 onKeyDown={(event) => {
                   if (event.key === "Enter") {
                     event.preventDefault();
