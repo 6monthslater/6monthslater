197511f78d2865c793f5c56a8d5ca09656e9de6d
Eric C <39015447+ricetech@users.noreply.github.com>
2023-11-23 16:14:50 -0500

    feat: restyle footer

--- server/app/components/footer.tsx
+++ server/app/components/footer.tsx
@@ -1,14 +1,16 @@
 const Footer = () => {
   return (
-    <div className="!mt-auto bg-gray-300 p-4 text-center">
+    <div className="!mt-auto border-t-2 bg-gray-100 p-4 text-center">
       <div>
         6 Months Later is licensed under AGPLv3, available on{" "}
         <a
+          className="text-blue-500 underline"
           href="https://www.github.com/6monthslater/6monthslater"
           rel="noopener"
         >
           GitHub
         </a>
+        .
       </div>
     </div>
   );
