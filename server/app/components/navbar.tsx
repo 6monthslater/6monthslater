0ae9100647dc93a3368677120ad4425b4ea18da0
Eric C <39015447+ricetech@users.noreply.github.com>
2023-11-23 12:18:41 -0500

    feat: restyle navbar

--- server/app/components/navbar.tsx
+++ server/app/components/navbar.tsx
@@ -1,11 +1,38 @@
-import { Link } from "@remix-run/react";
-import NavLink from "~/components/remix-ui/navlink";
+import { Link, useLocation, NavLink as RemixNavLink } from "@remix-run/react";
+import { NavLink } from "~/components/shadcn-ui/navlink";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/shadcn-ui/dropdown-menu";
+import { Button } from "~/components/shadcn-ui/button";
+import type { RemixNavLinkProps } from "@remix-run/react/dist/components";
+import type { ReactNode } from "react";
 
 interface NavbarProps {
   isLoggedIn: boolean;
 }
 
+interface DropdownNavLinkProps extends RemixNavLinkProps {
+  children: ReactNode;
+}
+
+const DropdownNavLink = (props: DropdownNavLinkProps) => {
+  return (
+    <RemixNavLink {...props}>
+      <DropdownMenuItem className="!cursor-pointer">
+        {props.children}
+      </DropdownMenuItem>
+    </RemixNavLink>
+  );
+};
+
 const Navbar = ({ isLoggedIn }: NavbarProps) => {
+  const location = useLocation();
+  const adminPath = new RegExp(/\/admin\/.*/);
+  const isAdminPage = adminPath.test(location.pathname);
+
   return (
     <nav className="!mt-0 flex flex-wrap items-center justify-between bg-gradient-to-r from-cyan-700 to-cyan-500 to-90% p-4 text-slate-200">
       <div className="mr-6 flex flex-shrink-0 items-center">
@@ -14,14 +41,36 @@ const Navbar = ({ isLoggedIn }: NavbarProps) => {
         </span>
       </div>
       <div className="block w-full flex-grow lg:flex lg:w-auto lg:items-center">
-        <div className="text-sm lg:flex-grow">
+        <div className="space-x-3 text-sm lg:flex-grow">
           <NavLink to="/" end>
             Home
           </NavLink>
-          <NavLink to="/admin/queue">Add to Queue</NavLink>
-          <NavLink to="/admin/queue-status">Queue Status</NavLink>
-          <NavLink to="/admin/crawler">Product Crawler</NavLink>
-          <NavLink to="/admin/reviews">View Reviews</NavLink>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                size="xs"
+                className={isAdminPage ? "active bg-gray-50 bg-opacity-25" : ""}
+              >
+                Admin Utilities
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownNavLink to={"/admin/queue"}>
+                Add to Queue
+              </DropdownNavLink>
+              <DropdownNavLink to={"/admin/queue-status"}>
+                Queue Status
+              </DropdownNavLink>
+              <DropdownNavLink to={"/admin/crawler"}>
+                Product Crawler
+              </DropdownNavLink>
+              <DropdownNavLink to={"/admin/reviews"}>
+                View Reviews
+              </DropdownNavLink>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
           {isLoggedIn ? (
             <NavLink className="float-right" to="/auth/logout">
               Log Out
---
+++ server/app/components/shadcn-ui/navlink.tsx
@@ -0,0 +1,19 @@
+import { NavLink as RemixNavLink } from "@remix-run/react";
+import type { RemixNavLinkProps } from "@remix-run/react/dist/components";
+import { buttonVariants } from "~/components/shadcn-ui/button";
+
+export const NavLink = (props: RemixNavLinkProps) => {
+  return (
+    <RemixNavLink
+      {...props}
+      className={({ isActive }) =>
+        `${props.className} ${buttonVariants({
+          variant: "ghost",
+          size: "xs",
+        })} ${isActive ? "active bg-gray-50 bg-opacity-25" : ""}`
+      }
+    >
+      {props.children}
+    </RemixNavLink>
+  );
+};
