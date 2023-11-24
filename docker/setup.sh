79f11fa219145c63e2f83cf6b33e05b4e143cb9f
Eric C <39015447+ricetech@users.noreply.github.com>
2023-11-24 10:35:58 -0500

    feat: create docker setup script

---
+++ docker/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp analyzer.env.example analyzer.env
+cp caddy.env.example caddy.env
+cp crawler.env.example crawler.env
+cp database.env.example database.env
+cp scraper.env.example scraper.env
+cp server.env.example server.env
+cp shared_scraper.env.example shared_scraper.env
