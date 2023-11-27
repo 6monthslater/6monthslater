1891c0fb320ca67ee4b492083280d3c55fa18995
Ajay Ramachandran <dev@ajay.app>
2023-11-26 23:46:37 -0500

    Center image vertically
    Co-authored-by: Eric C. <39015447+ricetech@users.noreply.github.com>

--- server/app/components/product-header.tsx
+++ server/app/components/product-header.tsx
@@ -13,6 +13,7 @@ const ProductHeader = ({ product }: ProductHeaderProps) => (
       <Card className="mx-auto grid w-full grid-cols-1 gap-1 space-y-4 md:h-full md:grid-cols-7 md:gap-6 lg:w-3/4">
         <span className="grid justify-center">
           <img
+            className="my-auto"
             src={product.image_url.replace("US120", "US240")}
             alt="Product"
             width={120}
