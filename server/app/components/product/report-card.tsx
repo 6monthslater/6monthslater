0238204ac11066c796c2f93e937be2e9088bef3a
Eric C <39015447+ricetech@users.noreply.github.com>
2023-11-29 01:13:03 -0500

    feat: style bottom half of report card

--- server/app/components/product/report-card.tsx
+++ server/app/components/product/report-card.tsx
@@ -3,7 +3,7 @@ import type { REPORT_INCLUDE } from "~/types/product";
 import { Card, DonutChart } from "@tremor/react";
 import { Badge } from "~/components/ui/badge";
 import { getSingleUnitDateInterval } from "~/utils/format";
-import { TbCircleCheck, TbThumbUpFilled } from "react-icons/tb";
+import { TbCircleCheck, TbThumbUp } from "react-icons/tb";
 import { Separator } from "~/components/ui/separator";
 
 type Report = Prisma.ReportGetPayload<{ include: typeof REPORT_INCLUDE }>;
@@ -127,32 +127,56 @@ export default function ReportCard({
             )
         )}
       </div>
-      <p>
-        <b>Date: </b>
-        {<>{report?.review?.date_text ?? report.createdAt}</>}
-      </p>
-      {report?.review?.verified_purchase ? (
-        <p>
-          <b>
-            <TbCircleCheck />
-            Verified Purchase
-          </b>
-        </p>
-      ) : null}
-      <p>
-        <TbThumbUpFilled />
-        {report?.review?.found_helpful_count}
-      </p>
-      {report?.review?.is_top_positive_review ? (
-        <p>
-          <b>Top Positive Review</b>
-        </p>
-      ) : null}
-      {report?.review?.is_top_critical_review ? (
-        <p>
-          <b>Top Critical Review</b>
-        </p>
-      ) : null}
+      <div className="grid grid-cols-3 items-center justify-center divide-x-2 text-center">
+        <div className="flex h-full flex-col items-center justify-center px-2">
+          {(() => {
+            const date = report?.review?.date ?? report.createdAt;
+            return (
+              <div className="flex flex-col items-center justify-center text-center">
+                <div className="text-sm font-semibold">Report Date</div>
+                <div className="text-xl font-semibold">
+                  {new Date(date).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "short",
+                  })}
+                </div>
+                <div>{new Date(date).getFullYear()}</div>
+              </div>
+            );
+          })()}
+        </div>
+        <div className="flex h-full flex-col items-center justify-center px-2 leading-snug">
+          {report?.review?.verified_purchase ? (
+            <>
+              <TbCircleCheck className="h-10 w-10 stroke-lime-500" />
+              <span>Verified Purchase</span>
+            </>
+          ) : null}
+        </div>
+        <div className="flex h-full items-center justify-center gap-2 px-2 text-2xl">
+          <TbThumbUp className="h-8 w-8 fill-blue-400 stroke-1" />
+          {report?.review?.found_helpful_count}
+        </div>
+      </div>
+      <div
+        hidden={
+          !report?.review?.is_top_critical_review &&
+          !report?.review?.is_top_positive_review
+        }
+        className="text-center"
+      >
+        <Separator className="my-2 h-0.5" />
+        {report?.review?.is_top_positive_review ? (
+          <p>
+            <b>Top Positive Review</b>
+          </p>
+        ) : null}
+        {report?.review?.is_top_critical_review ? (
+          <p>
+            <b>Top Critical Review</b>
+          </p>
+        ) : null}
+      </div>
     </Card>
   );
 }
